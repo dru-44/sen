16729315544db2407a7e893ff4f9fccc86c19112
dru-44 <dhruthik28@gmail.com>
2022-05-10 17:06:12 +0530

    ver++

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="Sentinel-imgpackage",
-    version="0.0.8",
+    version="0.0.9",
     author="Dru44",
     author_email="dhruthik28@gmail.com",
     license='MIT',
